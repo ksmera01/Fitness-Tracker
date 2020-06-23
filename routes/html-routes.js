8d019f0a840964ba9555721e6d641aad77ae18b1
Kevin Smeraglio <ksmera01@gmail.com>
2020-06-22 22:48:46 -0400

    updated html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,16 @@
+//HTML Routes
+
+const path = require("path");
+let express = require("express");
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname + "/public/stats.html"));
+});
